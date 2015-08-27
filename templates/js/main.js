e7d0657ddd6b4923fc7413e464077e98f5026177
Amos Kyler <amos.kyler@gmail.com>
2015-08-27 00:28:14 -0700

    funny little mouse thing

--- templates/js/main.js
+++ templates/js/main.js
@@ -1,13 +1,26 @@
 function main() {
   'use strict';
-  // console.log('welcome to my new site. I hope you stay for a bit');
-  // window.mouseLocation = document.createElement('div');
-  // window.mouseLocation.appendChild(document.createTextNode('x: 0 y: 0'));
-  // document.body.insertBefore(mouseLocation, document.body.firstChild);
-  // window.displayNavigation = function () {
-  //   window.mouseLocation.innerHTML = 'x: ' + event.x + ' y: ' + event.y;
-  //   window.mouseLocation.setAttribute('style', 'position: absolute; margin-top:'+event.y + '; margin-left:'+event.x+';');
-  // };
+  console.log('welcome to my new site. I hope you stay for a bit');
+  var mouseLocation = document.createElement('div');
+  mouseLocation.setAttribute('style', 'position: absolute;');
+  mouseLocation.appendChild(document.createTextNode(''));
+  document.body.insertBefore(mouseLocation, document.body.firstChild);
+  window.displayNavigation = function () {
+    var _x, _y;
+    if(screen.width/event.x > 3.8) {
+      _x = event.x - 40;
+      _y = event.y - 36;
+      mouseLocation.innerHTML = '<';
+      mouseLocation.setAttribute('style', 'position: absolute; top:'+_y+ 'px; left:'+_x+'px; font-size: 5em; color:#2ECC71');
+    } else {
+      mouseLocation.setAttribute('style', 'display:none;');
+    }
+  };
+  window.clickBack = function () {
+    if(screen.width/event.x > 3.8) {
+        window.history.back();
+    }
+  };
 }
 
 // Use jquery or some other solution to handle more browsers
