7cb3967e9c34b70afc4138044274b44a062f2394
Amos Kyler <amos.kyler@gmail.com>
2015-08-26 23:39:11 -0700

    prettifying

--- api/get.js
+++ api/get.js
@@ -1,20 +1,20 @@
+var Jade = require('jade');
+var path = require('path');
+var assign = require('object-assign');
+var config = require('../config');
 var Post = require('../schemas/post');
 
 module.exports = function (app) {
   app.get('/', function(req, res){
-    var adHocHTML = '<div>'
-    adHocHTML += '<h2>Recently Created!</h2>'
+    var template, adHocHTML;
     Post.find({})
-        .select('id title')
+        .select('id title subreddit')
         .limit(15)
         .exec(function (err, docs) {
           if(err != null) return res.status(500).send(err);
-          var reducedPosts = docs.reduce(function (prev, curr, idx) {
-            return prev + '<div><a href="'+curr.id+'">' + curr.title + '</a></div>'
-          }, '');
-          adHocHTML += reducedPosts;
-          adHocHTML += '</div>'
+          template = path.join(__dirname, '../templates/home.jade');
+          adHocHTML = Jade.renderFile(template, assign({recent: docs}, config));
           return res.send(adHocHTML);
         });
   });
-}
+};
--- templates/js/main.js
+++ templates/js/main.js
@@ -1,6 +1,13 @@
 function main() {
   'use strict';
-  console.log('welcome to my new site. I hope you stay for a bit');
+  // console.log('welcome to my new site. I hope you stay for a bit');
+  // window.mouseLocation = document.createElement('div');
+  // window.mouseLocation.appendChild(document.createTextNode('x: 0 y: 0'));
+  // document.body.insertBefore(mouseLocation, document.body.firstChild);
+  // window.displayNavigation = function () {
+  //   window.mouseLocation.innerHTML = 'x: ' + event.x + ' y: ' + event.y;
+  //   window.mouseLocation.setAttribute('style', 'position: absolute; margin-top:'+event.y + '; margin-left:'+event.x+';');
+  // };
 }
 
 // Use jquery or some other solution to handle more browsers
